f5f65ca22fbb9a57c9b5a189975e0ce311e50664
Aaron Barlow <aaron.barlow@progleasing.com>
2022-04-19 14:03:40 -0600

    role2

--- terraform/role.tf
+++ terraform/role.tf
@@ -21,3 +21,28 @@ resource "aws_iam_role" "test_role1" {
     tag-key = "tag-value"
   }
 }
+
+
+resource "aws_iam_role" "test_role2" {
+  name = "global_test_role2"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
